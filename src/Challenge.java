e668b8c4fb30d5512ecc92ad9398e00a652afe53
SerraSrcgl <raclawicka123@gmail.com>
2021-01-07 18:49:28 +0100

    commit

--- src/Challenge.java
+++ src/Challenge.java
@@ -1,3 +1,55 @@
 public class Challenge {
+    private int NumberOfPagesToRead;
+    private int NumberOfAlreadyReadPage;
+    private String dateDueChallenge;
 
+    public Challenge() {
+    }
+
+    public Challenge(int numberOfPagesToRead, int numberOfAlreadyReadPage, String dateDueChallenge) {
+        NumberOfPagesToRead = numberOfPagesToRead;
+        NumberOfAlreadyReadPage = numberOfAlreadyReadPage;
+        this.dateDueChallenge = dateDueChallenge;
+    }
+
+
+    public void addChallenge() {
+
+    }
+
+    public void completeChallenge() {
+
+    }
+
+    public void markProgress() {
+
+    }
+
+    public void deleteChallenge() {
+
+    }
+
+    public int getNumberOfPagesToRead() {
+        return NumberOfPagesToRead;
+    }
+
+    public void setNumberOfPagesToRead(int numberOfPagesToRead) {
+        NumberOfPagesToRead = numberOfPagesToRead;
+    }
+
+    public int getNumberOfAlreadyReadPage() {
+        return NumberOfAlreadyReadPage;
+    }
+
+    public void setNumberOfAlreadyReadPage(int numberOfAlreadyReadPage) {
+        NumberOfAlreadyReadPage = numberOfAlreadyReadPage;
+    }
+
+    public String getDateDueChallenge() {
+        return dateDueChallenge;
+    }
+
+    public void setDateDueChallenge(String dateDueChallenge) {
+        this.dateDueChallenge = dateDueChallenge;
+    }
 }
