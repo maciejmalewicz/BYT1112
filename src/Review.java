444cb1f1f5f3890be8bcdcc9718fcaead88bb406
olokos <olokos@wp.pl>
2021-01-07 22:36:29 +0100

    Complete Review class with its test

--- src/Review.java
+++ src/Review.java
@@ -1,12 +1,59 @@
+import java.util.Calendar;
+import java.util.Date;
+
 public class Review {
 
-    private String ISBN;
-    private String contentDescription;
+    private int reviewID = 0;
+    private final String ISBN;
+    private final String contentDescription;
+    private int whoWroteUID;
+    private Date dateOfUpdate;
 
     public Review(String ISBN, String content){
+        this.reviewID= ++reviewID;
+        this.ISBN = ISBN;
         this.contentDescription=content;
+        //TODO: once all the parts is ready here's where we'll link user with his review
+        // this.whoWroteUID=
+        this.dateOfUpdate= Calendar.getInstance().getTime();
     }
 
+    public String readReview(){
+    return "Review of a book";
+    }
 
+    public void deleteReview(int deletedID){
+    };
 
+    public int getReviewID() {
+        return reviewID;
+    }
+
+    public void setReviewID(int reviewID) {
+        this.reviewID = reviewID;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public String getContentDescription() {
+        return contentDescription;
+    }
+
+    public int getWhoWroteUID() {
+        return whoWroteUID;
+    }
+
+    public void setWhoWroteUID(int whoWroteUID) {
+        this.whoWroteUID = whoWroteUID;
+    }
+
+    public Date getDateOfUpdate() {
+        return dateOfUpdate;
+    }
+
+    public void setDateOfUpdate(Date dateOfUpdate) {
+        this.dateOfUpdate = dateOfUpdate;
+    }
 }
--- test/ReviewTest.java
+++ test/ReviewTest.java
@@ -1,5 +1,41 @@
+import org.junit.Assert;
 import org.junit.Test;
 
-public class ReviewTest {
+import java.util.Calendar;
+import java.util.Date;
 
+public class ReviewTest {
+    @Test
+    public void canCreateReview(){
+        Review testReview = new Review("1","Some review ...");
+        Assert.assertNotNull(testReview);
+    }
+    @Test
+    public void getterTest(){
+        Review testReview = new Review("2","Some another review");
+        Assert.assertEquals(1,testReview.getReviewID());
+        //TODO: LINK Assert.assertEquals(1,testReview.getWhoWroteUID());
+        Assert.assertEquals("Some another review",testReview.getContentDescription());
+        Assert.assertTrue("Dates aren't close enough to each other!", (Calendar.getInstance().getTime().compareTo(testReview.getDateOfUpdate()) ) < 1000);
+        Assert.assertEquals("2",testReview.getISBN());
+
+    }
+    @Test
+    public void setterTest(){
+        Review testReview = new Review("3","Third review contents");
+
+        Assert.assertEquals(1,testReview.getReviewID());
+        testReview.setReviewID(5);
+        Assert.assertEquals(5,testReview.getReviewID());
+
+        Assert.assertEquals(0,testReview.getWhoWroteUID());
+        testReview.setWhoWroteUID(5);
+        Assert.assertEquals(5,testReview.getWhoWroteUID());
+
+        Calendar currentTimeAnd24H =Calendar.getInstance();
+        currentTimeAnd24H.add(Calendar.HOUR,24);
+        testReview.setDateOfUpdate(currentTimeAnd24H.getTime());
+        Assert.assertTrue("Dates aren't close enough to each other!", (currentTimeAnd24H.getTime().compareTo(testReview.getDateOfUpdate()) ) < 1000);
+
+    }
 }
