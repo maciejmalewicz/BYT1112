f3af04f4c2cfaf2e22d251766ee955051b19fdbf
SerraSrcgl <raclawicka123@gmail.com>
2021-01-07 18:41:03 +0100

    commit

--- src/Owner.java
+++ src/Owner.java
@@ -1,5 +1,37 @@
 public class Owner {
-    public int userId;
-    public int bookId;
+    private int userId;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void recieveBorrowedBook() {
+    }
+
+    public void registerBook() {
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public Owner(int userId, int bookId) {
+        this.userId = userId;
+        this.bookId = bookId;
+    }
+
+    private int bookId;
+
+    public Owner() {
+    }
+
 
 }
