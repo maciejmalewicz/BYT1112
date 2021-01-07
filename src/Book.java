32d7530cd72663f1c842f726810985db70fbbb76
yzrsamet <yzr.samet@gmail.com>
2021-01-07 18:58:25 +0100

    Create Book,BookBorrowing Classes

---
+++ src/Book.java
@@ -0,0 +1,59 @@
+package src;
+
+public class Book {
+
+    private int BookId;
+    private int ISBN;
+    private String name;
+    private String image;
+
+    public int getBookId() {
+        return BookId;
+    }
+
+    public void setBookId(int bookId) {
+        BookId = bookId;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private Review review;
+    private BookDescription description;
+    private Note note;
+    private User user;
+    private Owner owner;
+
+
+
+
+    public void AddNote(String note){
+
+    }
+    public void AddReview(String note){
+
+    }
+    public void AddPhoto(){
+
+    }
+    public void SetChallenge(){
+
+    }
+    public void BarrowBookToSB(){
+
+    }
+
+}
---
+++ src/BookBorrowing.java
@@ -0,0 +1,28 @@
+package src;
+
+import java.util.Date;
+
+public class BookBorrowing {
+    private Date dateLent;
+    private Date  dateBarrow;
+
+    public void CheckDueDates(){
+
+    }
+
+    public Date getDateLent() {
+        return dateLent;
+    }
+
+    public void setDateLent(Date dateLent) {
+        this.dateLent = dateLent;
+    }
+
+    public Date getDateBarrow() {
+        return dateBarrow;
+    }
+
+    public void setDateBarrow(Date dateBarrow) {
+        this.dateBarrow = dateBarrow;
+    }
+}
