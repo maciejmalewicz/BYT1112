1148a48ee45f6fb39185a8d883d55b61bd64d391
Maciej Malewicz <maciekmalewicz2012b@gmail.com>
2021-01-14 18:28:35 +0100

    Connections corrected part 1

--- src/ReadingProgress.java
+++ src/ReadingProgress.java
@@ -4,14 +4,18 @@ public class ReadingProgress {
 
     public ReadingProgress(String ISBN, int noOfPagesRead){
         this.pagesRead=noOfPagesRead;
+        this.ISBN = ISBN;
 
     }
     public void updateProgress(int noOfPagesRead){
         this.pagesRead=pagesRead+noOfPagesRead;
     }
+
     public void deleteProgress(){
         this.pagesRead=0;
     }
+
+
     public int getPagesRead() {
         return pagesRead;
     }
