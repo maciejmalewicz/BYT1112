b98eec7da3561d4edcd25b18131422c7bc42ef54
Maciej Malewicz <maciekmalewicz2012b@gmail.com>
2021-01-16 15:53:53 +0100

    Macieks tests finished

--- test/NoteTest.java
+++ test/NoteTest.java
@@ -7,21 +7,36 @@ import java.util.Date;
 
 public class NoteTest {
 
+    public Date testDate;
     public Note tested;
 
     @Before
     public void setup(){
-        tested = new Note("AAASDC", "Very interesting", new Date(), new User(1l));
+        testDate = new Date();
+        tested = new Note("AAASDC", "Very interesting", testDate, new User(1L));
     }
 
     @Test
-    public void testGetContent(){
+    public void testConstruction(){
+        Assert.assertEquals(tested.getISBN(), "AAASDC");
+        Assert.assertEquals(tested.getContentNote(), "Very interesting");
+        Assert.assertEquals(tested.getDateOfUpdate(), testDate);
+        Assert.assertEquals(tested.getAddedBy().getUserID(), Long.valueOf(1L));
+    }
+
+    @Test
+    public void testGetters(){
         TestUtils.testGetter(tested, "getContentNote", "contentNote");
         TestUtils.testGetter(tested, "getISBN", "ISBN");
+        TestUtils.testGetter(tested, "getDateOfUpdate", "dateOfUpdate");
+        TestUtils.testGetter(tested, "getAddedBy", "addedBy");
     }
 
     @Test
-    public void testSetContent(){
-        TestUtils.testSetter(tested, "setContentNote", "contentNote", "mienso");
+    public void testSetters(){
+        TestUtils.testSetter(tested, "setContentNote", "contentNote", "anything");
+        TestUtils.testSetter(tested, "setISBN", "ISBN", "AABBCC");
+        TestUtils.testSetter(tested, "setDateOfUpdate", "dateOfUpdate", new Date());
+        TestUtils.testSetter(tested, "setAddedBy", "addedBy", new User(50L));
     }
 }
--- test/UserTest.java
+++ test/UserTest.java
@@ -1,4 +1,46 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class UserTest {
 
+
+    User tested;
+
+    @Before
+    public void setup(){
+        tested = new User(10L);
+        tested.setName("Andrzej");
+        tested.setSurname("Papuch");
+    }
+
+    @Test
+    public void testConstructor(){
+        Assert.assertEquals(tested.getUserID(), Long.valueOf(10L));
+    }
+
+    @Test
+    public void testGetters(){
+        TestUtils.testGetter(tested, "getName", "name");
+        TestUtils.testGetter(tested, "getSurname", "surname");
+        TestUtils.testGetter(tested, "getNotes", "notes");
+        TestUtils.testGetter(tested, "getChallenges", "challenges");
+        TestUtils.testGetter(tested, "getReviews", "reviews");
+        TestUtils.testGetter(tested, "getBookBorrowings", "bookBorrowings");
+        TestUtils.testGetter(tested, "getReadingProgresses", "readingProgresses");
+    }
+
+    @Test
+    public void testSetters(){
+        TestUtils.testSetter(tested, "setName", "name", "Arnold");
+        TestUtils.testSetter(tested, "setSurname", "surname", "Krawczyk");
+        TestUtils.testSetter(tested, "setNotes", "notes", new ArrayList<>());
+        TestUtils.testSetter(tested, "setChallenges", "challenges", new ArrayList<>());
+        TestUtils.testSetter(tested, "setReviews", "reviews", new ArrayList<>());
+        TestUtils.testSetter(tested, "setBookBorrowings", "bookBorrowings", new ArrayList<>());
+        TestUtils.testSetter(tested, "setReadingProgresses", "readingProgresses", new ArrayList<>());
+    }
 
 }
